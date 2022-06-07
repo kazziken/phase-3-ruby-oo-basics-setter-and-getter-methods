2476ff8efe439495a1c337dfe28596c52d69f578
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-06-07 16:54:14 -0400

    commit tree 1a6508637ed9a5e2189ae0bfae9de1a630fc0818

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
 
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(input)
+        @job = input
+    end
+
+
+    def job
+        @job
+    end
+
+
 end
